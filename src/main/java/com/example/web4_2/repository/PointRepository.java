d1f6f1644ded2df7aa560428ec08a1c898868173
p11menova <p11menova@yandex.ru>
2025-04-26 19:02:10 +0300

    finally working custom-build task

--- src/main/java/com/example/web4_2/repository/PointRepository.java
+++ src/main/java/com/example/web4_2/repository/PointRepository.java
@@ -3,7 +3,6 @@ package com.example.web4_2.repository;
 import com.example.web4_2.models.Point;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
 
 public interface PointRepository extends CrudRepository<Point, Long> {
 }

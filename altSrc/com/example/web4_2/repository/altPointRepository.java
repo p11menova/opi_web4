70c1f93df2727a4a4c6063c3288c685cf6f5ff3d
p11menova <p11menova@yandex.ru>
2025-04-27 23:47:38 +0300

    added history task

--- altSrc/com/example/web4_2/repository/altPointRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.web4_2.repository;
-
-import com.example.web4_2.models.altPoint;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface altPointRepository extends CrudRepository<altPoint, Long> {
-}
